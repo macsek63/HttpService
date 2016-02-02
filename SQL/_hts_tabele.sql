d1da7f56d2bd1510f3428a40ac4b4eb58bcf1d2b
Maciej Sękowski <MAC@simple.local>
2016-02-02 15:34:12 +0100

    Signed-off-by: Maciej Sękowski <MAC@simple.local>

--- SQL/_hts_tabele.sql
+++ SQL/_hts_tabele.sql
@@ -1,6 +1,4 @@
--- tabela  procedur dostêpnych dla serwisu http
---
---
+--tabela procedur dostêpnych dla serwisu http
 IF OBJECT_ID('hts_dostepne_procedury', 'U') IS NULL
 begin
 	print 'nowa tabela: [hts_dostepne_procedury]'
